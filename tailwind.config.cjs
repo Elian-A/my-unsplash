e3372266f5a4491b9cd51e139b7d51572594eef0
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-11 19:46:49 -0300

    Add global css variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,23 @@ module.exports = {
       fontFamily: {
         sans: "var(--ff-base)",
         accent: "var(--ff-accent)",
+      },
+      fontSize:{
+        md: 'var(--fs-md)',
+        lg: 'var(--fs-lg)',
+        xl: 'var(--fs-xl)',
+      },
+      colors:{
+        primary: {
+          500: "var(--clr-primary-500)"
+        },
+        secondary: {
+          400: "var(--clr-secondary-400)"
+        },
+        neutral: {
+          100: "var(--clr-neutral-100)",
+          700: "var(--clr-neutral-700)",
+        }
       }
     },
   },
