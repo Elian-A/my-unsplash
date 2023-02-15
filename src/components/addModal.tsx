ecdb2b1f48ffc1d967c1aa04bdb5f5cebc9816bc
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-15 04:08:07 -0300

    Creat error handler for inputs

--- src/components/addModal.tsx
+++ src/components/addModal.tsx
@@ -1,22 +1,76 @@
+import { useCallback, useContext, useState } from "react";
+import type { FormEventHandler } from "react";
+
+import { z } from "zod";
 import FormInput from "./formInput";
+import type { FormError } from "./modal";
+import { ModalContext } from "../context/modalContext";
+
+const modalValidator = z.object({
+  label: z.string().min(1).max(20),
+  url: z.string().url(),
+});
 
 const AddModal = () => {
+  const [errors, setErrors] = useState<FormError[]>([]);
+  const { toggleModalView } = useContext(ModalContext);
+
+  const handleSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    const formElement = e.target as HTMLFormElement;
+    const formInputs = Object.fromEntries(new FormData(formElement));
+    const validatedform = modalValidator.safeParse(formInputs);
+    if (!validatedform.success) {
+      const zodErrors = validatedform.error.errors.map(({ path, message }) => ({
+        message,
+        path: path.toString(),
+      }));
+      setErrors(zodErrors);
+    }
+
+    /* TODO: 
+           Send to backend 
+           Close modal
+    */
+  };
+
+  const findError = useCallback(
+    (label: string) => errors.find((err) => err.path === label) || null,
+    [errors]
+  );
+
   return (
     <article className="grid gap-4 font-sans">
       <h3 className="text-xl">Add new photo</h3>
-      <div className="grid gap-4">
-        <FormInput label="Label" placeholder="Suspendisse elit massa" />
+      <form className="grid gap-4" onSubmit={handleSubmit}>
         <FormInput
-          label="Url"
-          placeholder="https://images.unsplash.com/photo-1676348879583-fd7e0fd5514c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+          label="label"
+          placeholder="Suspendisse elit massa"
+          error={findError("label")}
+          setErrors={setErrors}
         />
-      </div>
-      <div className="flex justify-end gap-8 align-middle">
-        <button className="text-sm text-neutral-600">cancel</button>
-        <button className="rounded-xl bg-primary-500 px-5 py-3 font-bold text-white">
-          Submit
-        </button>
-      </div>
+        <FormInput
+          label="url"
+          placeholder="https://images.unsplash.com/photo-1676348879583-fd7e0fd5514c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+          error={findError("url")}
+          setErrors={setErrors}
+        />
+        <div className="flex justify-end gap-8 align-middle">
+          <button
+            type="button"
+            className="text-sm text-neutral-600"
+            onClick={() => toggleModalView(false)}
+          >
+            cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-xl bg-primary-500 px-5 py-3 font-bold text-white"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     </article>
   );
 };
--- src/components/formInput.tsx
+++ src/components/formInput.tsx
@@ -1,21 +1,39 @@
-import type { FC } from "react";
+import { useEffect } from "react";
+import type { FC, Dispatch, SetStateAction } from "react";
+import type { FormError } from "./modal";
 
-const FormInput: FC<{ label: string; placeholder: string }> = ({
+interface FormInputInterface {
+  label: string;
+  placeholder: string;
+  error: FormError | null;
+  setErrors: Dispatch<SetStateAction<FormError[]>>;
+}
+
+const FormInput: FC<FormInputInterface> = ({
   label,
   placeholder,
-}) => (
-  <div className="grid gap-1 ">
-    <label htmlFor={label} className="text-md">
-      {label}
-    </label>
-    <input
-      type="text"
-      id={label}
-      name={label}
-      placeholder={placeholder}
-      className="text-medium text-ellipsis rounded-xl border border-neutral-700 p-3"
-    />
-  </div>
-);
+  error,
+  setErrors,
+}) => {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setErrors([]), 3000);
+    return () => clearTimeout(timeoutId);
+  }, [error, setErrors]);
+  return (
+    <div className="grid gap-1 ">
+      <label htmlFor={label} className="text-md">
+        {label}
+      </label>
+      <input
+        type="text"
+        id={label}
+        name={label}
+        placeholder={placeholder}
+        className="text-medium text-ellipsis rounded-xl border border-neutral-700 p-3"
+      />
+      {error ? <p className="text-sm text-red-500">{error.message}</p> : null}
+    </div>
+  );
+};
 
 export default FormInput;
