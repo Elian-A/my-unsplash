e493011c7a8ecb095a9f80050e6195fd7de8019d
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-16 00:35:04 -0300

    Fill imageContext with photos Use context to render photos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,28 @@
 import type { Photo } from "@prisma/client";
 import type { GetServerSidePropsContext, NextPage } from "next";
 import type { Session } from "next-auth";
+import { useContext, useEffect } from "react";
 import Layout from "../components/layout";
 import Photos from "../components/photos";
+import { ImageContext } from "../context/imageContext";
 import { getServerAuthSession } from "../server/auth";
 import { getUserPhotos } from "../utils/photo";
 
 const Home: NextPage<{ authData: Session; userPhotos: Photo[] }> = ({
   userPhotos,
 }) => {
+  const { addPhotos, photos } = useContext(ImageContext);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => addPhotos(userPhotos), []);
+
   return (
     <Layout title="My unsplash | Home">
       <div className="grid items-center">
         {userPhotos.length === 0 ? (
           <h2>You have no photos :c</h2>
         ) : (
-          <Photos photos={userPhotos} />
+          <Photos photos={photos} />
         )}
       </div>
     </Layout>
