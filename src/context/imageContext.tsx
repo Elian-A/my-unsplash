af99516a83273a1451b6151046402995da8c987a
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-16 00:34:00 -0300

    Modify image context a bit

--- src/context/imageContext.tsx
+++ src/context/imageContext.tsx
@@ -3,25 +3,23 @@ import type { FC, ReactElement } from "react";
 import { createContext, useState } from "react";
 interface ImageContext {
   photos: Photo[];
-  addPhoto: (photo: Photo) => void;
+  addPhotos: (photo: Photo[]) => void;
   deletePhoto: (photo: Photo) => void;
 }
 
 export const ImageContext = createContext<ImageContext>({
   photos: [],
-  addPhoto: () => null,
+  addPhotos: () => null,
   deletePhoto: () => null,
 });
 
 export const ImageProvider: FC<{ children: ReactElement }> = ({ children }) => {
   const [photos, setPhotos] = useState<Photo[]>([]);
-  const addPhoto = (photo: Photo) => {
-    setPhotos([...photos, photo]);
-  };
-  const deletePhoto = (photo: Photo) => {
+  const addPhotos = (addedPhotos: Photo[]) =>
+    setPhotos([...photos, ...addedPhotos]);
+  const deletePhoto = (photo: Photo) =>
     setPhotos(photos.filter(({ id }) => id !== photo.id));
-  };
-  const value = { photos, addPhoto, deletePhoto };
+  const value = { photos, addPhotos, deletePhoto };
   return (
     <ImageContext.Provider value={value}>{children}</ImageContext.Provider>
   );
