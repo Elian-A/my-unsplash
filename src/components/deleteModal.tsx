ae9342749946c57bd0fd0bd4cd0fce362dc50ba0
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-17 01:54:24 -0300

    Finish deleteModal styling

--- src/components/deleteModal.tsx
+++ src/components/deleteModal.tsx
@@ -1,5 +1,53 @@
+import { useCallback, useContext, useState } from "react";
+import type { FormEventHandler } from "react";
+import { ImageContext } from "../context/imageContext";
+import { ModalContext } from "../context/modalContext";
+import type { FormError } from "../types";
+import FormInput from "./formInput";
+
 const DeleteModal = () => {
-  return <article>ar</article>;
+  const [errors, setErrors] = useState<FormError[]>([]);
+  const { toggleModalView } = useContext(ModalContext);
+  // const { addPhotos } = useContext(ImageContext);
+
+  const handleSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const findError = useCallback(
+    (label: string) => errors.find((err) => err.path === label) || null,
+    [errors]
+  );
+
+  return (
+    <article className="grid gap-4 font-sans">
+      <h3 className="text-xl">Are you sure?</h3>
+      <form className="grid gap-4" onSubmit={handleSubmit}>
+        <FormInput
+          label="password"
+          type="password"
+          placeholder="*******************"
+          error={findError("label")}
+          setErrors={setErrors}
+        />
+        <div className="flex justify-end gap-8 align-middle">
+          <button
+            type="button"
+            className="text-sm text-neutral-600"
+            onClick={() => toggleModalView(false)}
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-xl bg-secondary-400 px-5 py-3 font-bold text-white"
+          >
+            Delete
+          </button>
+        </div>
+      </form>
+    </article>
+  );
 };
 
 export default DeleteModal;
