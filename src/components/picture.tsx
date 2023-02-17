92c9f199fc2dcad49a98cf958f4ebb33005ce049
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-17 01:13:36 -0300

    Finish styling

--- src/components/picture.tsx
+++ src/components/picture.tsx
@@ -4,11 +4,12 @@ import type { FC } from "react";
 
 const Picture: FC<{ photo: Photo }> = ({ photo }) => {
   const { url, label } = photo;
+  const isLarge = Math.random() > 0.5;
   return (
     <div
-      className={`relative ${
-        Math.random() > 0.5 ? "row-span-2" : ""
-      } overflow-auto rounded-xl shadow-lg shadow-slate-500`}
+      className={`group relative cursor-pointer overflow-auto rounded-xl shadow-lg shadow-slate-500  hover:scale-110 hover:rounded-2xl ${
+        isLarge ? "row-span-2" : ""
+      } flex flex-col`}
     >
       <Image
         src={url}
@@ -16,8 +17,19 @@ const Picture: FC<{ photo: Photo }> = ({ photo }) => {
         style={{ objectFit: "cover", objectPosition: "center" }}
         alt={label}
         sizes="100%"
+        className="group-hover:brightness-75"
         priority={true}
       />
+      <div className="relative z-10 mt-4 mr-4 self-end">
+        {/* I dont know why next-font isn't working,
+         fw-500 is not accesible even when is especified in _app  */}
+        <button className="rounded-3xl border-2 border-secondary-400 py-1 px-5 font-accent text-sm text-secondary-400 contrast-125 hover:scale-110 hover:contrast-200">
+          delete
+        </button>
+      </div>
+      <h3 className="relative z-10 mt-auto cursor-default break-words px-6 pb-6 font-sans text-xl font-bold text-white">
+        {label}
+      </h3>
     </div>
   );
 };
