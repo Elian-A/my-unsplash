1be2b2da88e5f21aeeb264703079ec5014378fa4
Elian Avalos <elianemanuelstudy@gmail.com>
2023-03-15 21:19:22 -0300

    Add delete to image services

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -12,3 +12,16 @@ export const postImage = async (image: { label: string; url: string }) => {
   const photo = (await postRes.json()) as Photo;
   return photo;
 };
+
+export const deleteImage = async (id: string) => {
+  const deleteRes = await fetch(baseUrl, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ id }),
+  });
+
+  const resObject = deleteRes.json();
+  return resObject;
+};
